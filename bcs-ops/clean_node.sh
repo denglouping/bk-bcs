32130f5d152d1749a91898f92dbce6fc0837e36a
denglouping <359150423@qq.com>
2025-07-30 18:43:58 +0800

    feat: 打印清理网卡日志

--- bcs-ops/clean_node.sh
+++ bcs-ops/clean_node.sh
@@ -51,8 +51,10 @@ clean_cni() {
 }
 
 clean_vni() {
+  ifconfig cni0
   ip l | awk '/cni0|kube-ipvs0/ && !/veth/ {eth=$2;gsub(":","",eth);print eth}' | xargs -r -n 1 ip l d
   ip l | awk '/veth/{eth=$2;split(eth,a,"@");print a[1]}' | xargs -r -n 1 ip l d
+  ifconfig cni0
 }
 
 utils::log "INFO" "reseting kubelet"
